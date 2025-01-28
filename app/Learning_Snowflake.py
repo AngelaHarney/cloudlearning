bcd28f61ff93f6880dcd9fe4e260c39951f235d0
AngelaHarney <AngelaMarieHarney@gmail.com>
2025-01-28 14:12:44 -0800

    blue

--- app/Learning_Snowflake.py
+++ app/Learning_Snowflake.py
@@ -176,7 +176,7 @@ with st.sidebar:
     st.button("Update points") 
     st.write('')
     st.write('')
-    st.write("*:black[Author:  Angela Harney, Snowflake Data Superhero*")       
+    st.write("*:blue[Author:  Angela Harney, Snowflake Data Superhero*")       
 
 
 #--------------------------------------------------------------------
